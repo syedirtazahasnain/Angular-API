dfdcf1cd44bef8d93f8681c4d6296c80f9ebd0ed
Irtaza <arsal.h@yahoo.com>
2024-12-31 02:26:21 +0500

    making component , router link , and bootstrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { UserComponent } from './component/user/user.component';
+import { AdminComponent } from './component/admin/admin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent,AdminComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './component/user/user.component';
+import { AdminComponent } from './component/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "user-page",
+    component: UserComponent
+  },
+  {
+    path:"admin-page",
+    component: AdminComponent
+  }
+];
